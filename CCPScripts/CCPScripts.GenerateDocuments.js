aa3607d0bc9f6623a6146572d140b0d16dd05472
simon8029 <simon.lau@msn.com>
2017-12-14 00:26:54 -0500

    generate all component files array with absolute path

--- CCPScripts/CCPScripts.GenerateDocuments.js
+++ CCPScripts/CCPScripts.GenerateDocuments.js
@@ -6,48 +6,91 @@ Chokidar = require('chokidar');
 Parse = ReactDocgen.parse;
 
 var paths = {
-  sample: Path.join(__dirname, '../src', 'CCPDocuments', 'Sample'),
-  components: Path.join(__dirname, '../src', 'CCPComponents'),
-  output: Path.join(__dirname, '../CCPSettings', 'CCPComponentsResources.js')
+  SampleFolder: Path.join(__dirname, '../src', 'CCPDocuments', 'Samples'),
+  ComponentsFolder: Path.join(__dirname, '../src', 'CCPComponents'),
+  OutputFolder: Path.join(__dirname, '../CCPSettings', 'CCPComponentsResources.js')
 };
 
 const enableWatchMode = process.argv.slice(2) == '--watch';
 if (enableWatchMode) {
-  // Regenerate component metadata when components or sample change.
-  Chokidar.watch([paths.sample, paths.components]).on('change', function (event, path) {
-    generate(paths);
+  // Regenerate component metadata when ComponentsFolder or sample change.
+  Chokidar.watch([paths.SampleFolder, paths.ComponentsFolder]).on('change', function (event, path) {
+    generateComponentMetaData(paths.ComponentsFolder);
   });
 } else {
   // Generate component metadata
-  generate(paths);
+  // generateComponentMetaData(paths.ComponentsFolder);
+  var allComponentFiles = [];
+  allComponentFiles = getAllComponentFilesWithAbsolutePath(paths.ComponentsFolder, allComponentFiles);
+  allComponentFiles = removeFilesWithGivenCharacters(allComponentFiles, ['index.js', 'spec.js']);
+  allComponentFiles.map(f => {
+    console.log(Chalk.green(f));
+    // console.log(f.indexOf('index.js') === -1);
+  })
 }
 
-function generate(paths) {
+function removeFilesWithGivenCharacters(fileArray, charactersArray) {
+  reduced = fileArray.reduce(function (filteredFileArray, file) {
+    var includeGivenKeywords = charactersArray.map(c => {
+      return file.indexOf(c) !== -1;
+    }).includes(true);
+
+    if (!includeGivenKeywords) {
+      filteredFileArray.push(file);
+    }
+    return filteredFileArray;
+  }, []);
+
+  return reduced;
+};
+
+function getAllComponentFilesWithAbsolutePath(folderWithAbsolutePath, componentFiles) {
+  var files = getFilesWithAbsolutePath(folderWithAbsolutePath).map((file) => {
+    componentFiles.push(file);
+  });
+
+  getSubFolderWithAbsolutePath(folderWithAbsolutePath).map(function (subFolder) {
+    getAllComponentFilesWithAbsolutePath(subFolder, componentFiles
+    );
+  });
+  return componentFiles;
+};
+
+function generateComponentMetaData(folderWithAbsolutePath) {
   var errors = [];
-  var componentSources = getDirectories(paths.components).map(function (componentName) {
+  let componentFiles = [];
+  var componentSources = getSubFolderWithAbsolutePath(folderWithAbsolutePath).map(function (item) {
     try {
-      return getComponentSoucomponentSources(paths, componentName)
+      var files = getFilesWithAbsolutePath(`${folderWithAbsolutePath}\\${item}`).map((file) => {
+        componentFiles.push(`${folderWithAbsolutePath}\\${item}\\${file}`);
+      });
+      var subFolders = getSubFolderWithAbsolutePath(item).map((folderWithAbsolutePath) => {
+
+      });
+
+      // generateComponentMetaData(folderWithAbsolutePath);
+
     } catch (error) {
-      errors.push('Error: ' + componentName + '. ' + error);
+      errors.push('Error: ' + item + '=>' + error);
     }
   });
-  writeFile(paths.output, "module.exports = " + JSON.stringify(errors.length ? errors : componentSources));
+  // writeFile(paths.OutputFolder, "module.exports = " + JSON.stringify(errors.length ? errors : componentSources));
 }
 
-function getComponentSoucomponentSources(paths, componentName) {
-  var content = readFile(Path.join(paths.components, componentName, componentName + '.js'));
+function populateComponentModel(paths, componentName) {
+  var content = readFile(Path.join(paths.ComponentsFolder, componentName, componentName + '.js'));
   var info = parse(content);
   return {
     name: componentName,
     description: info.description,
     props: info.props,
     code: content,
-    sample: getSample(paths.sample, componentName)
+    SampleFolder: getSamples(paths.SampleFolder, componentName)
   }
 }
 
-function getSample(samplePath, componentName) {
-  var sample = getExampleFiles(samplePath, componentName);
+function getSamples(samplePath, componentName) {
+  var sample = getSampleFiles(samplePath, componentName);
   return sample.map(function (file) {
     var filePath = Path.join(samplePath, componentName, file)
     var content = readFile(filePath)
@@ -60,31 +103,38 @@ function getSample(samplePath, componentName) {
   });
 }
 
-function getExampleFiles(samplePath, componentName) {
+function getSampleFiles(samplePath, componentName) {
   var exampleFiles = [];
   try {
-    exampleFiles = getFiles(path.join(samplePath, componentName));
+    exampleFiles = getFilesWithAbsolutePath(path.join(samplePath, componentName));
   } catch (error) {
-    console.log(Chalk.yellow(`${componentName} doesn't have any sample yet.`));
   }
   return exampleFiles;
 }
 
-function getDirectories(filepath) {
-  return Fs.readdirSync(filepath).filter(function (file) {
-    return Fs.statSync(Path.join(filepath, file)).isDirectory();
+// Given folderpath, return all subfolder's absolute path
+function getSubFolderWithAbsolutePath(folderPath) {
+  var folderNames = Fs.readdirSync(folderPath).filter(function (item) {
+    return Fs.statSync(Path.join(folderPath, item)).isDirectory();
   });
+
+  return folderNames.map(folderName => {
+    return `${folderPath}\\${folderName}`;
+  })
 }
 
-function getFiles(filepath) {
-  return Fs.readdirSync(filepath).filter(function (file) {
-    return Fs.statSync(Path.join(filepath, file)).isFile();
+function getFilesWithAbsolutePath(folderPath) {
+  var fileNames = Fs.readdirSync(folderPath).filter(function (item) {
+    return Fs.statSync(Path.join(folderPath, item)).isFile();
+  });
+
+  return fileNames.map(fileName => {
+    return `${folderPath}\\${fileName}`;
   });
 }
 
 function writeFile(filepath, content) {
   Fs.writeFile(filepath, content, function (err) {
-    err ? console.log(Chalk.red(err)) : console.log(Chalk.green("Components resources generated."));
   });
 }
 
--- CCPSettings/CCPSettings.Paths.js
+++ CCPSettings/CCPSettings.Paths.js
@@ -4,8 +4,6 @@ const path = require('path');
 const fs = require('fs');
 const url = require('url');
 
-// Make sure any symlinks in the project folder are resolved:
-// https://github.com/facebookincubator/create-react-app/issues/637
 const appDirectory = fs.realpathSync(process.cwd());
 const resolveApp = relativePath => path.resolve(appDirectory, relativePath);
 
@@ -40,7 +38,7 @@ function getServedPath(appPackageJson) {
 
 // config after eject: we're in ./config/
 module.exports = {
-  dotenv: resolveApp('.env'),
+  dotenv: resolveApp('CCPSettings.Environment'),
   appBuild: resolveApp('build'),
   appPublic: resolveApp('public'),
   appHtml: resolveApp('public/index.html'),
