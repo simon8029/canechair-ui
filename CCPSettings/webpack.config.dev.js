45fffe109891941cb170fc7660b9f7bd543798af
simon8029 <simon.lau@msn.com>
2017-12-23 11:34:54 -0500

    integrate webpack-dashboard

--- CCPSettings/webpack.config.dev.js
+++ CCPSettings/webpack.config.dev.js
@@ -1,8 +1,16 @@
 const commonConfig = require('./webpack.config');
+const DashboardPlugin = require('webpack-dashboard/plugin')
 module.exports = Object.assign({}, commonConfig, {
   devServer: {
     stats: "errors-only",
     host: process.env.HOST,
-    port: process.env.PORT
-  }
+    port: process.env.PORT,
+    overlay: {
+      errors: true,
+      warnings: true
+    }
+  },
+  plugins: [
+    new DashboardPlugin()
+  ]
 });
